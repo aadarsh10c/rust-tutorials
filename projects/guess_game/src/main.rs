a58474852f5f141000f74eab02769da4c65c1920
Adarsh Kumar <adarsh010c@gmail.com>
2023-05-30 00:24:34 +0530

    Added loop, completed my first rust program

--- projects/guess_game/src/main.rs
+++ projects/guess_game/src/main.rs
@@ -8,23 +8,34 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is : {secret_number}");
     
-    println!("Please input your guess.");
+    loop{
+        println!("Please input your guess.");
 
-    // Variable to store the user input in
-    let mut guess= String::new();
+        // Variable to store the user input in
+        let mut guess= String::new();
 
-    io::stdin()//create a handle on the input
-        .read_line(&mut guess)//method used to capture user data
-        .expect("Failed to read line");
+        io::stdin()//create a handle on the input
+            .read_line(&mut guess)//method used to capture user data
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number");
+                continue;
+            }
+        };
 
-    println!("You guessed: {guess}");
+        // println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too Small!!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You Win !!")
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too Small!!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You Win !!");
+                break;
+            }
+        }
     }
 
 
